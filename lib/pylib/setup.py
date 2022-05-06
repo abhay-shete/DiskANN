a0bcbb2ff0b6cdb4ca8f813df7b944fbea016d4a
Abhay Shete <abhay.shete@kainos.com>
2022-05-06 14:53:41 +0100

    setup.py for installing the python module

---
+++ lib/pylib/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='diskann',
+      version='1.0',
+      description='Python DiskANN Bindings',
+      author='Abhay Shete',
+      author_email='abhay.shete@kainos.com',
+      url="https://www.kainos.com",
+      py_modules=['diskann'],
+     )
